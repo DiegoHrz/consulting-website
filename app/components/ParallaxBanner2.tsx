c585cb04e614164dd52f2415bcdf2e32bd722b87
DiegoHrz <hernandezwdiego@gmail.com>
2025-01-13 13:30:10 -0500

    Add test

--- app/components/ParallaxBanner2.tsx
+++ app/components/ParallaxBanner2.tsx
@@ -6,76 +6,87 @@ interface ParallaxBannerProps {
   height: string | number;
 }
 
-const ParallaxBanner4: React.FC<ParallaxBannerProps> = ({
-  imageUrl = '/banner/parallax-image-2.jpg',
+const ParallaxBanner: React.FC<ParallaxBannerProps> = ({
+  imageUrl='/banner/parallax-image-2.jpg',
   height,
 }) => {
-  const [isSafari, setSafari] = useState(false);
+  const [isBrowserSafari, setBrowserIsSafari] = useState(false);
 
   useEffect(() => {
-    // Detecta cualquier versión de Safari
-    const isSafariBrowser = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    setSafari(isSafariBrowser);
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) {
+      setBrowserIsSafari(true);
+    }
   }, []);
 
-  if (isSafari) {
-    return (
-      <div className="relative h-[27rem] overflow-hidden">
-        <div
-          className="absolute inset-0 w-full h-full bg-cover bg-center"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-            transform: 'translate3d(0,0,0)',
-            willChange: 'transform'
-          }}
-        />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-anna-white">
-          <img
-            src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
-            alt="White Logo"
-            className="h-auto w-40 mx-auto px-3 animate-fade-title"
-          />
-          <p className="font-cabin text-3xl">Bereit für die Veränderung?</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div
-      style={{
-        position: "relative",
-        height: typeof height === "number" ? `${height}rem` : height,
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-          backgroundAttachment: "fixed",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          zIndex: -1,
-        }}
-      />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-anna-white">
-        <img
-          src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
-          alt="White Logo"
-          className="h-auto w-40 mx-auto px-3 animate-fade-title"
-        />
-        <p className="font-cabin text-3xl">Bereit für die Veränderung?</p>
-      </div>
+    <div>
+      {isBrowserSafari ? (
+        <div className="h-[27rem]">
+          <div
+            className="border h-64 w-full bg-transparent"
+            style={{
+              content: '""',
+              position: 'fixed',
+              bottom: 0,
+              left: '50%',
+              transform: 'translateX(-50%)',
+              width: '150vh',
+              height: '100vh',
+              backgroundImage: `url(${imageUrl})`,
+              zIndex: -100,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center 10%',
+              overflowY: 'hidden',
+            }}
+          >
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-anna-white">
+            <img
+              src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
+              alt="White Logo"
+              className="h-auto w-40 mx-auto px-3 animate-fade-title"
+            />
+            <p className="font-cabin text-3xl">Bereit für die Veränderung?</p>
+          </div>
+        </div>
+      ) : (
+        <div
+          style={{
+            position: "relative",
+            height: typeof height === "number" ? `${height}rem` : height,
+            overflow: "hidden",
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+              backgroundAttachment: "fixed",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: -1,
+            }}
+          />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-anna-white">
+            <img
+              src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
+              alt="White Logo"
+              className="h-auto w-40 mx-auto px-3 animate-fade-title"
+            />
+            <p className="font-cabin text-3xl">Bereit für die Veränderung?</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default ParallaxBanner4;
+export default ParallaxBanner;
 
 // "use client";
 // import React, { useEffect, useState } from "react";
