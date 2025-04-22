5bc5e61e17a1eb1f260499ae6170e3047c257db8
AnnaSSchenk <Anna.schenk0212@googlemail.com>
2025-04-21 20:39:56 -0700

    8:39

--- app/components/BusinessPlans.tsx
+++ app/components/BusinessPlans.tsx
@@ -21,19 +21,18 @@ const BusinessPlans = () => {
           text="Verlässlichkeit, Erfahrung und echtes Engagement – wir begleiten dich auf deinem Weg"
           title={false}
         />
-
       </div>
       <div className="text-black flex flex-col md:flex-row w-full justify-between gap-8  md:gap-2 lg:gap-10 max-w-96 sm:max-w-max mx-auto  ">
         <div className="flex flex-col  items-center gap-3 p-6 md:p-4  flex-1  border-[#D5E0E2] rounded-xl bg-white md:shadow-gray-300 md:shadow-[0px_4px_2px_0px] min-h-56 lg:min-h-64 justify-center">
           <GoShieldCheck size={40} color="#581D1D" />
           <h6 className="font-cabin text-2xl md:text-xl lg:text-2xl text-center">
-          100% Erfolgsrate
+            100% Erfolgsrate
           </h6>
           <p className="font-original_text text-base lg:text-lg text-anna-gray text-center max-w-80 py-2">
-          Jeder einzelne Antrag auf Gründerzuschuss unserer Kunden wurde genehmigt.
+            Jeder einzelne Antrag auf Gründerzuschuss unserer Kunden wurde
+            genehmigt.
           </p>
         </div>
-
 
         {/* <div className="w-[0.07rem] bg-black "></div> */}
 
@@ -41,11 +40,10 @@ const BusinessPlans = () => {
         <div className="flex flex-col  items-center gap-3 p-6 md:p-4  flex-1  border-[#D5E0E2] rounded-xl bg-white shadow-[#125369] shadow-[0px_3px_10px_0px] min-h-56 lg:min-h-64 justify-center">
           <SlCalender size={40} color="#581D1D" />
           <h6 className="font-cabin text-2xl md:text-xl lg:text-2xl text-center">
-          Kostenlose Beratung
+            Kostenlose Beratung
           </h6>
           <p className="font-original_text text-base lg:text-lg text-center max-w-80 py-2">
-            Try it out for 100 days. If you don’t love it, send it back. Learn
-            more.
+            Buche ein unverbindliches Erstberatungsgespräch mit uns.
           </p>
         </div>
         {/* <div className="w-[0.07rem] bg-black "></div> */}
