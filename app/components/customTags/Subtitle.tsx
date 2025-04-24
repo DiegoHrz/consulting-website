bea701f95396fdf80e746a9c89b85afccd454ebb
AnnaSSchenk <Anna.schenk0212@googlemail.com>
2025-04-24 09:57:42 -0700

    Add uber-uns finished

--- app/components/customTags/Subtitle.tsx
+++ app/components/customTags/Subtitle.tsx
@@ -3,12 +3,13 @@ interface SubtitleProps {
   color?: string;
   position?: string;
   size?:string
+  mobileSize?:string
 }
 
-const Subtitle: React.FC<SubtitleProps> = ({ text, color = "anna-white", position='center',size='xl' }) => {
+const Subtitle: React.FC<SubtitleProps> = ({ text, color = "anna-white", position='center',size='xl',mobileSize }) => {
   return (
     <div
-      className={`text-${position} text-${color} font-vollkorn font-medium text-${size}`}
+      className={`text-${position} text-${color} font-vollkorn font-medium ${mobileSize?`${mobileSize} sm:text-${size}`:`text-${size}`} `}
     >
       {text}
     </div>
